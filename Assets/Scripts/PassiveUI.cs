4805df3639ea21cd76b23e299143c4ed2c09d59a
Jaakko Liukkala <jaakko.liukkala@cs.tamk.fi>
2017-10-18 21:59:50 +0300

    Some basic PassiveUI stuff done. Includes a PassiveUI prefab that has a TextWriterEffect script which shows the text letter by letter.

--- Assets/Scripts/PassiveUI.cs
+++ Assets/Scripts/PassiveUI.cs
@@ -9,7 +9,9 @@ public class PassiveUI : MonoBehaviour {
     private Text _description;
     private string[] text;
     private int position = 0;
+    private bool isAtEnd;
 
+    // Makes a getter and a setter for the string array variable text.
     public string[] Text
     {
         private get {
@@ -22,14 +24,41 @@ public class PassiveUI : MonoBehaviour {
         }
     }
 
+    // Makes a getter and a setter for the boolean variable isAtEnd.
+    public bool IsAtEnd
+    {
+        private get {
+            return isAtEnd;
+        }
+
+        set {
+            isAtEnd = value;
+        }
+    }
+
+    // If an input key is pressed, the Scroll method is called.
     public void HandleInput(string input) {
-
+        if (input == ("Select"))
+        {
+            Scroll();
+        } 
     }
+
+    // Initiates the description text and sets it in the first position.
     private void Initiate() {
-
+        _description.text = text[position];
     }
-    private void Scroll() {
 
+    // Scrolls to the next position.
+    private void Scroll() {
+        if (text.Length == position + 1)
+        {
+
+        } else
+        {
+            position = position + 1;
+            _description.text = text[position];
+        }
     }
 
 
---
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour {
+
+    // How long it takes for the next letter to show up.
+    public float delay = 0.1f;
+    // The entire text.
+    [SerializeField]
+    private string fullText;
+    // The letter at the current position.
+    private string currentText = "";
+
+	void Start () {
+        StartCoroutine(ShowText());
+	}
+	
+    IEnumerator ShowText()
+    {
+        // Prints out the text letter by letter.
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay); 
+        }
+    }
+}
